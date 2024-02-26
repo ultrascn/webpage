8d8c8e232f59727885e1e919bc04d0fecf100e27
Jan Pecha <janpecha@email.cz>
2024-02-26 18:18:49 +0100

    WebSite: added $favicon

--- src/LogoImage.php
+++ src/LogoImage.php
@@ -5,48 +5,6 @@
 	namespace UltraScn\Webpage;
 
 
-	/**
-	 * @property-read non-empty-string $path
-	 * @property-read positive-int|NULL $width
-	 * @property-read positive-int|NULL $height
-	 */
-	class LogoImage
+	class LogoImage extends Image
 	{
-		/** @var non-empty-string */
-		private $path;
-
-		/** @var positive-int|NULL */
-		private $width;
-
-		/** @var positive-int|NULL */
-		private $height;
-
-
-		/**
-		 * @param non-empty-string $path
-		 * @param int|NULL $width
-		 * @param int|NULL $height
-		 */
-		public function __construct(
-			$path,
-			$width,
-			$height
-		)
-		{
-			assert($width === NULL || $width > 0);
-			assert($height === NULL || $height > 0);
-
-			$this->path = $path;
-			$this->width = $width;
-			$this->height = $height;
-		}
-
-
-		/**
-		 * @return mixed
-		 */
-		public function __get(string $prop)
-		{
-			return $this->{$prop};
-		}
 	}
--- src/WebSite.php
+++ src/WebSite.php
@@ -9,6 +9,7 @@
 	 * @property-read non-empty-string $name
 	 * @property-read non-empty-string|NULL $motto
 	 * @property-read Logo $logo
+	 * @property-read Image|NULL $favicon
 	 */
 	class WebSite
 	{
@@ -21,21 +22,27 @@
 		/** @var Logo */
 		private $logo;
 
+		/** @var Image|NULL */
+		private $favicon;
+
 
 		/**
 		 * @param non-empty-string $name
 		 * @param non-empty-string|NULL $motto
 		 * @param Logo $logo
+		 * @param Image|NULL $favicon
 		 */
 		public function __construct(
 			$name,
 			$motto,
-			Logo $logo
+			Logo $logo,
+			?Image $favicon = NULL
 		)
 		{
 			$this->name = $name;
 			$this->motto = $motto;
 			$this->logo = $logo;
+			$this->favicon = $favicon;
 		}
 
 
